343f1cba925eba23da39772e22d18a19a7146917
Luana Ketely <83798719+Luanaketelygp@users.noreply.github.com>
2023-05-02 19:39:22 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -4,12 +4,17 @@ import time
 
 HOST = '127.0.0.1'
 PORT = 5000
-MIN_LENGTH = 1
-MAX_LENGTH = 999999999999999999999999999999
-RANDOM_NUMBER = str(randint(MIN_LENGTH, MAX_LENGTH))
+MIN_DIGITS = 1
+MAX_DIGITS = 30
 TIME_EXECUTE = 10
 
+def random_number_with_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+                   
 while True:
+    RANDOM_NUMBER = str(random_number_with_digits(randint(MIN_DIGITS, MAX_DIGITS)))
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
     s.sendall(str.encode(RANDOM_NUMBER))
